c61aad14dbd51a6f08877209691b0183f46e6c69
DeFi_Nurturer <146621475+FabianUmo@users.noreply.github.com>
2024-08-26 09:21:32 +0100

    Create Nurturer.sh

---
+++ Nurturer.sh
@@ -0,0 +1,7 @@
+echo "# Swisstronik" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/FabianUmo/Swisstronik.git
+git push -u origin main
